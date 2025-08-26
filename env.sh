672ca349030c18f1b9a97db0e1a5dd62dba2db93
Jonathan Lukas <jonathan.lukas@camunda.com>
2025-08-26 08:55:11 +0200

    initial project

---
+++ env.sh
@@ -0,0 +1,5 @@
+# Self-Managed
+export ZEEBE_ADDRESS='zeebe.consulting-sandbox.camunda.cloud:443'
+export ZEEBE_CLIENT_ID='zeebe'
+export ZEEBE_CLIENT_SECRET='9GS6hTh8e6ux3rmZ0qr8hcBEGcsJ8qUM'
+export CAMUNDA_OAUTH_URL='https://keycloak.consulting-sandbox.camunda.cloud/realms/jonny/protocol/openid-connect/token'
